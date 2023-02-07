a6d8e1acb9eb18fb97e6e3631de84add70f23d43
bayupamuji <bayuteguhpamuji@gmail.com>
2023-02-07 08:11:21 +0700

    modify readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from "react";
 function Header({ onSearch }) {
   return (
     <div className="note-app__header">
-      <h1>Note App</h1>
-      <input type="text" placeholder="Search" onChange={onSearch} />
+      <h1>Notes</h1>
+      <div className="note-search">
+        <input type="text" placeholder="Search" onChange={onSearch} />
+      </div>
     </div>
   );
 }
